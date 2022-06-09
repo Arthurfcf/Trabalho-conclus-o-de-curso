c40c1d859aebdc30aa47ff36d557331b52905a3a
Arthur Fernando Carvalho Ferreira <arthur.ferreira@compasso.com.br>
2022-06-09 18:43:38 -0300

    first commit

---
+++ Api.Data/Migrations/20220609211555_UserMigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class UserMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateAt = table.Column<DateTime>(nullable: true),
+                    UpdateAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 60, nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "CreateAt", "Email", "Name", "UpdateAt" },
+                values: new object[] { new Guid("234e1ca1-15c0-4047-a39d-71ae0d889a2a"), new DateTime(2022, 6, 9, 18, 15, 55, 70, DateTimeKind.Local).AddTicks(1213), "administrador@gmail", "Administrador", new DateTime(2022, 6, 9, 18, 15, 55, 70, DateTimeKind.Local).AddTicks(7513) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                table: "User",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
