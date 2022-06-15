9828f3ed0080d086edf0f74b1e31a240167fbe72
Arthur Fernando Carvalho Ferreira <arthur.ferreira@compasso.com.br>
2022-06-15 10:28:45 -0300

    Adcionando Testes API Service

---
+++ ServiceTest/Usuario/QuandoForExecutadoGet.cs
@@ -0,0 +1,24 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ServiceTest.Usuario
+{
+    public class QuandoForExecutadoGet : UsuariosTestes
+    {
+        private IUserService _service;
+
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É Possivel Executar o Metodo GET." )]
+
+        public abstract Task E_Possivel_Executar_Metodo_Get()
+        {
+            _serviceMock = new Mock<IUserService>();
+        } 
+    }
+}
---
+++ ServiceTest/Usuario/UsuariosTestes.cs
@@ -0,0 +1,87 @@
+﻿using Api.Domain.Dtos.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceTest.Usuario
+{
+    public class UsuariosTestes
+    {
+        public static string NomeUsuario { get; set; }
+        
+        public static string EmailUsuario { get; set; }
+
+        public static string NomeAlterado { get; set; }
+
+        public static string EmailAlterado { get; set; }
+
+        public static Guid IdUsuario { get; set; }
+
+        public List<UserDto> listaUserDto = new List<UserDto>();
+
+        public UserDto userDto;
+
+        public UserDtoCreate userDtoCreate;
+
+        public UserDtoCreateResult userDtoCreateResult;
+
+        public UserDtoUpdate userDtoUpdate;
+
+        public UserDtoUpdateResult userDtoUpdateResult;
+
+        public UsuariosTestes()
+        {
+            IdUsuario = Guid.NewGuid();
+            NomeUsuario = Faker.Name.FullName();
+            EmailUsuario = Faker.Internet.Email();
+            NomeAlterado = Faker.Name.FullName();
+            EmailAlterado = Faker.Internet.Email();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var dto = new UserDto()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = Faker.Name.FullName(),
+                    Email = Faker.Internet.Email(),
+                };
+                listaUserDto.Add(dto);
+
+                userDto = new UserDto
+                {
+                    Id = IdUsuario,
+                    Name = NomeUsuario,
+                    Email = EmailUsuario
+                };
+                userDtoCreate = new UserDtoCreate
+                {
+                    Name = NomeUsuario,
+                    Email = EmailUsuario
+                };
+
+                userDtoCreateResult = new UserDtoCreateResult
+                {
+                    Id = IdUsuario,
+                    Name = NomeUsuario,
+                    Email = EmailUsuario,
+                    CreateAt=DateTime.UtcNow
+                };
+
+                userDtoUpdate = new UserDtoUpdate
+                {
+                    Id = IdUsuario,
+                    Name = NomeUsuario,
+                    Email = EmailUsuario
+                };
+
+                userDtoUpdateResult = new UserDtoUpdateResult
+                {
+                    Id= IdUsuario,
+                    Name = NomeUsuario,
+                    Email = EmailUsuario,
+                    UpdateAt = DateTime.UtcNow
+                };
+            }
+        }
+    }
+}
