48d96658c80a0879addfb6661ef622409db52ffc
Arthur Fernando Carvalho Ferreira <arthur.ferreira@compasso.com.br>
2022-06-15 12:23:30 -0300

    Implementando Teste Unitario GetId camada service

--- ServiceTest/Usuario/QuandoForExecutadoGet.cs
+++ ServiceTest/Usuario/QuandoForExecutadoGet.cs
@@ -1,8 +1,7 @@
-﻿using Api.Domain.Interfaces.Services.User;
+﻿using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
 using Moq;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -16,9 +15,22 @@ namespace ServiceTest.Usuario
 
         [Fact(DisplayName = "É Possivel Executar o Metodo GET." )]
 
-        public abstract Task E_Possivel_Executar_Metodo_Get()
+        public async Task E_Possivel_Executar_Metodo_Get()
         {
             _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup( x => x.Get(IdUsuario)).ReturnsAsync(userDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Get(IdUsuario);
+            Assert.NotNull(result);
+            Assert.True(result.Id == IdUsuario);
+            Assert.Equal(NomeUsuario, result.Name);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(x => x.Get(It.IsAny<Guid>())).Returns(Task.FromResult((UserDto)null));
+            _service = _serviceMock.Object;
+            var _record = await _service.Get(IdUsuario);
+            Assert.Null(_record);
         } 
     }
 }
